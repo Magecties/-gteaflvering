27466ff055e4d55a9b226eef28836d03696fc373
mauaf1 <magn6506@gmail.com>
2023-11-10 14:08:07 +0100

    sd

--- script.js
+++ script.js
@@ -29,7 +29,23 @@ d3.json("/albums.json").then(function(data) {
         .delay((track, i) => i * 20) // Delay each circle's animation by its index
         .attr("cy", track => yScale(track.timesPlayed)); // Animate to the final y position
 
-    // ... rest of your code for axes and labels ...
+    // Definere akserne til x og y (læg mærke til de to typer!):
+    const xAxis = d3.axisBottom().scale(xScale).ticks(5);
+    const yAxis = d3.axisLeft().scale(yScale).ticks(5);
+
+    // Lægge akserne til SVG-elementet:
+    svg
+        // Først laves en svg-group med "g"
+        .append("g")
+        // Så flyttes den til bunden af grafen
+        .attr("transform", "translate(0," + (h - padding) + ")")
+        // Magi som får akserne til at blive tegnet
+        .call(xAxis);
+
+    svg
+        .append("g")
+        .attr("transform", "translate(" + padding + ",0)")
+        .call(yAxis);
 }).catch(error => {
     console.error("Error loading the data: ", error);
 });
