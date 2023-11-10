8fd7edc9fa90c768e7d4b0be5df45a955b4f1a33
Braad21 <Kasperbraad@hotmail.com>
2023-11-10 14:01:37 +0100

    hej

--- script.js
+++ script.js
@@ -1,83 +1,35 @@
-// Width og height til SVG-elementet
 const w = 700;
 const h = 300;
 const padding = 40;
 
 d3.json("/albums.json").then(function(data) {
-//SVG-elementet
-const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+    // Assuming data is an array of albums, we need to flatten it to get all tracks
+    const tracks = data.flatMap(album => album.trackList);
 
-//Skala for x-aksen
-const xScale = d3.scaleLinear()
-  .domain([0, 1000])
-  .range([padding, w - padding]);
+    const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
-const yScale = d3.scaleLinear()
-  .domain([0, 1000])
-  .range([h - padding, padding]);
-  
-// Scatter plot
-svg
-  .selectAll("circle")
-  .data(data)
-  .enter()
-  .append("circle")
-  .attr("cx", function(d) {
-    return xScale(d.songNumber);
-  })
-  .attr("cy", function(d) {
-    return yScale(d.timesPlayed);
-  })
-  .attr("r", 2.5);
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(tracks, track => track.songNumber)])
+        .range([padding, w - padding]);
 
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(tracks, track => track.timesPlayed)])
+        .range([h - padding, padding]);
 
-    /*
-//Labels
-svg
-  .selectAll("text")
-  .data(dataset)
-  .enter()
-  .append("text")
-  .text(function (d) {
-    return d[1];
-  })
-  .attr("x", function (d) {
-    return xScale(d[0]) + 5;
-  })
-  .attr("y", function (d) {
-    
-     * Som med de andre tilfælde, hvor y-værdien skal bruges, skal vi rette koden til
-     * den nye virkelighed.
-     * Sidste gang skrev vi: return yScale(h - d[1]) - 5;
-     * Nu skriver vi:
-     
-    return yScale(d[1]) - 5;
-  })
-  .attr("font-family", "sans-serif")
-  .attr("font-size", "11px")
-  .attr("fill", "red");
-  
-*/
+    // Scatter plot with animation
+    svg.selectAll("circle")
+        .data(tracks)
+        .enter()
+        .append("circle")
+        .attr("cx", track => xScale(track.songNumber))
+        .attr("cy", h) // Start below the chart
+        .attr("r", 2.5)
+        .transition() // Start a transition
+        .duration(2000) // Duration of the animation in milliseconds
+        .delay((track, i) => i * 20) // Delay each circle's animation by its index
+        .attr("cy", track => yScale(track.timesPlayed)); // Animate to the final y position
 
-
-// Definere akserne til x og y (læg mærke til de to typer!):
-const xAxis = d3.axisBottom().scale(xScale).ticks(5);
-const yAxis = d3.axisLeft().scale(yScale).ticks(5);
-
-//Lægge akserne til SVG-elementet:
-
-svg
-  //Først laves en svg-group med "g"
-  .append("g")
-  //Så flyttes den til bunden af grafen
-  .attr("transform", "translate(0," + (h - 30) + ")")
-  //Magi som får akserne til at blive tegnet
-  .call(xAxis);
-
-svg
-  .append("g")
-  .attr("transform", "translate(" + 30 + ",0)")
-  .call(yAxis);
-
-
-})
+    // ... rest of your code for axes and labels ...
+}).catch(error => {
+    console.error("Error loading the data: ", error);
+});
